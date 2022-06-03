92fa09bde8acbd7a6113a6e2281e76c2c6d29ca8
laperlej <jonathan.laperle@usherbrooke.ca>
2022-06-03 09:40:04 -0400

    setup jest

---
+++ openfoam_cbp/__tests__/editpanel.test.jsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { EditPanel } from '../components/edit/editpanel'
+import { warningFilter } from '../lib/warningfilter'
+
+const originalWarn = console.warn.bind(console.warn)
+beforeAll(() => {
+  const patternList = [
+    'Found ReflexContainer with width=0, this will cause invalid behavior',
+    'vertical reflex-container'
+  ]
+  warningFilter(console, originalWarn, patternList)
+})
+
+afterAll(() => {
+  console.warn = originalWarn
+})
+
+describe('EditPanel', () => {
+  it('renders with empty context', () => {
+    render(<EditPanel />)
+  })
+})
